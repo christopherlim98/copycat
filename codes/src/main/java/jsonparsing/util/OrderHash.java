d1b059f58a3087891c6cd08e6d7bcdfb23f6b8b5
Kho Eng Hock Kenneth <enghock.kho.2019@sis.smu.edu.sg>
2020-11-08 17:40:45 +0800

    Update OrderHash.java

--- codes/src/main/java/jsonparsing/util/OrderHash.java
+++ codes/src/main/java/jsonparsing/util/OrderHash.java
@@ -1,6 +1,18 @@
 package jsonparsing.util;
 
 public class OrderHash {
-    // We care about order
-    // Cyclic shift
+    // We care about order (Polynomial)
+    /* Experiments have shown that 33, 37, 39, and 41 are particularly good choices for a 
+    when working with character strings that are English words. 
+    In fact, in a list of over 50,000 English words, taking a to be 33, 37, 39, or 41 produced less than 7 collisions 
+    in each case.*/
+    public static Long polynomialHash(String s) {
+        Long h = 0L;
+        int a = 33;
+        for (int i = 0 ; i < s.length() ; i++) {
+            h *= a;
+            h += ((int)s.charAt(i));
+        }
+        return h;
+    }
 }
