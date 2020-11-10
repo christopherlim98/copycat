d115757966daab7962b77009d5707464f5b60dd8
christopherlim98 <christopherlim98@gmail.com>
2020-11-10 19:34:53 +0800

    Change algorithm for expensive plagiarism checker

--- codes/src/main/java/jsonparsing/copycat/Worker.java
+++ codes/src/main/java/jsonparsing/copycat/Worker.java
@@ -5,7 +5,7 @@ import java.util.*;
 import jsonparsing.entity.AbstractSyntaxTree;
 import jsonparsing.util.Algorithm;
 import jsonparsing.util.JaroWinkler;
-import jsonparsing.util.JaroWinklerJob;
+import jsonparsing.constants.Constants;
 
 public class Worker {
     public double compare(AbstractSyntaxTree ast1, AbstractSyntaxTree ast2){
@@ -23,18 +23,61 @@ public class Worker {
      * Note: each level captures the code logic hashed as a single string.
      */
 
-    public double compareKeySets(Set<Integer> ks1, Set<Integer> ks2,
+     public double compareKeySets(Set<Integer> ks1, Set<Integer> ks2,
                                  HashMap<Integer, String> hm1, HashMap<Integer, String> hm2  ){
         // Sensitive to order
         double score = 0.0;
-        int count = 1;
+        int count = 0;
         for (Integer key : ks1){
-            JaroWinklerJob jw = new JaroWinklerJob();
-            score += jw.compute(hm1.get(key),hm2.get(key));
-            // score += jw.similarity(hm1.get(key),hm2.get(key));
+            score += compareInsensitiveToOrder(hm1.get(key), hm2.get(key));
             count++;
         }
 
+        // The depth of the tree correlates to the logic behind the code.
+        // As we want the final score to be sensitive to the differences number of logic levels,
+        // we take the square value of the fraction when normalising the score.
+        return score/count * ((double)Math.pow(ks1.size(),2) /Math.pow(ks2.size(),2));
+    }
+
+    public double compareInsensitiveToOrder(String s1, String s2){
+        double score = 0.0;
+        int [] charDiff = new int[Constants.HASHDICT.size()];
+        int [] charTotal = new int[Constants.HASHDICT.size()];
+        int differences = 0;
+        for (int i = 0; i < s1.length(); i++){
+            int index = s1.charAt(i) - 'a';
+            charDiff[index]++;
+            charTotal[index]++;
+        }
+
+        for (int i = 0; i < s2.length(); i++){
+            int index = s2.charAt(i) - 'a';
+            charDiff[index]--;
+            charTotal[index]++;
+        }
+
+        int totalWeight = 0;
+        for (int i = 0; i < charTotal.length ; i ++){
+            char ch = (char)('a' + i);
+            System.out.println(ch);
+            differences += Math.abs(charDiff[i]) * Constants.HASHWEIGHTS.get(ch);
+            totalWeight += Math.abs(charTotal[i]) * Constants.HASHWEIGHTS.get(ch);
+        }
+        int similarity = totalWeight - differences;
+        score = (double)similarity/totalWeight;
+        return score;
+    }
+
+    public double compareKeySetsSensitiveToOrder(Set<Integer> ks1, Set<Integer> ks2,
+                                 HashMap<Integer, String> hm1, HashMap<Integer, String> hm2  ){
+        // Sensitive to order
+        double score = 0.0;
+        int count = 0;
+        for (Integer key : ks1){
+            JaroWinkler jw = new JaroWinkler();
+            score += jw.similarity(hm1.get(key), hm2.get(key));
+            count++;
+        }
         // The depth of the tree correlates to the logic behind the code.
         // As we want the final score to be sensitive to the differences number of logic levels,
         // we take the square value of the fraction when normalising the score.
@@ -58,7 +101,6 @@ public class Worker {
             hm1, hm2);
         }
         return score;
-
     }
 
 }
