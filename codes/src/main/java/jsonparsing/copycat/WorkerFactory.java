536afe4e42f5caedf7aef4f63dded7cb825c85b2
christopherlim98 <christopherlim98@gmail.com>
2020-11-11 21:43:33 +0800

    Clean up code base and repackaged from jsonparsing to copycat

--- codes/src/main/java/jsonparsing/copycat/WorkerFactory.java
+++
@@ -1,7 +0,0 @@
-package jsonparsing.copycat;
-import jsonparsing.entity.AbstractSyntaxTree;
-
-public interface WorkerFactory{
-    public double compare(AbstractSyntaxTree ast1, AbstractSyntaxTree ast2);
-    
-}
--- codes/src/main/java/jsonparsing/exception/JsonToTreeTimeoutException.java
+++
@@ -1,7 +0,0 @@
-package jsonparsing.exception;
-
-public class JsonToTreeTimeoutException extends Exception {
-   public JsonToTreeTimeoutException(String message) {
-        super(message);
-   }
-}
